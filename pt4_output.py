26087ce82b5854bf1d60bb3b455714dca7975ca4
zhouzhibin <zhouzhibin@xiaomi.com>
2015-03-24 18:26:46 +0800

    add function to read a duration without peak value

--- pt4_output.py
+++ pt4_output.py
@@ -9,7 +9,10 @@ class Pt4Out(file):
         for smpl in Pt4FileReader.readAsVector(file):
             #print smpl[2].mainCurrent
             self.curlist5000.append(smpl[2].mainCurrent)
+        
         self.curlist100 = [self.curlist5000[i] for i in range(len(self.curlist5000)) if i%50 == 0]
+        #求1秒钟的平均电流做一次list，好处是减少后面不必要的计算
+        self.curpersec = [sum(self.curlist5000[i*5000:(i+1)*5000])/5000 for i in range(len(self.curlist5000)/5000)]
         #print self.curlist100
 
     def pt4getAveCur(self, sec):
@@ -18,7 +21,6 @@ class Pt4Out(file):
         """
         return "%.2f" % float(sum(self.curlist100[int(sec)*100:])/(self.curlist100.__len__() - int(sec)*100))
 
-    
     def pt4getAveHighThan(self, curbase):
         """在电流值高过某个值之后就开始计算平均电流
             curbase - 给定的开始记录的门槛值，取高于门槛值的点到文件末尾部分的电流平均值
@@ -40,7 +42,26 @@ class Pt4Out(file):
         return -1
     
     def pt4getSleepCurNoPeak(self, curbase, duration, startpos=0):
-        return -1
+        """从某个时间点起，电流低于一定数值，并且一直保持，在一定的时间段内没有跳变的平均电流
+            curbase - 电流门槛值，如果startpos没有设置的话，从第一个小于该电流值的开始计算平均值
+            duration - 统计区间的时常
+            startpos - 强制指定跳过的时间长度，弱该项参数指定后，从指定点之后寻找符合curbase要求的点
+        """
+        curl = self.curpersec[:]
+        length = len(curl)
+        sum = curl.pop()
+        ave = 0
+        if startpos > 0:
+            curl = self.curpersec[int(startpos):]
+        for i in range(len(curl)):
+            sec = curl.pop()
+            if sec >= curbase:
+                ave = sum/(i+1)
+                return ave
+            else:
+                sum += sec
+        ave = sum/(length + 1)
+        return ave
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
